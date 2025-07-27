ccf601ac9410e4e25f5ada52bcee0e7c9d307f96
kensidiangco <kensidiangco@gmail.com>
2025-07-27 17:50:52 +0800

    adding function on incrementing and decrement pouch quantity

--- api/serializers.py
+++ api/serializers.py
@@ -10,9 +10,29 @@ class PouchSerializer(serializers.ModelSerializer):
 class PouchInSerializer(serializers.ModelSerializer):
     class Meta: 
         model = models.Pouch_In
-        fields = '__all__'
+        fields = ['id', 'pouch', 'quantity', 'date_created']
+
+    def create(self, validated_data):
+        pouch = validated_data['pouch']
+        added_quantity = validated_data['quantity']
+        
+        # increment pouch quantity
+        pouch.quantity += added_quantity
+        pouch.save()
+
+        return super().create(validated_data)
 
 class PouchOutSerializer(serializers.ModelSerializer):
     class Meta: 
         model = models.Pouch_Out
         fields = '__all__'
+
+    def create(self, validated_data):
+        pouch = validated_data['pouch']
+        added_quantity = validated_data['quantity']
+        
+        # increment pouch quantity
+        pouch.quantity -= added_quantity
+        pouch.save()
+
+        return super().create(validated_data)
